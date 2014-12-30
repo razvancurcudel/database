dc7270b4077cd7f18013352e7881acce2a62f122
Martin Schröder <m.schroeder2007@gmail.com>
2014-12-30 15:40:09 +0100

    Integrated bin with new migration manager.

---
+++ src/Migration/MigrationManager.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of KoolKode Database.
+ *
+ * (c) Martin Schröder <m.schroeder2007@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KoolKode\Database\Migration;
+
+class MigrationManager
+{
+	public function handleCommand()
+	{
+		print_r($_SERVER);
+	}
+}
